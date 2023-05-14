22f07c07d6b5e9a19b27eea87d07a49e66295656
Brady Stroud <bradystroud@ssw.com.au>
2023-05-15 07:09:42 +1000

    🎨  Format code

--- lib/NumberTiles.dart
+++ lib/NumberTiles.dart
@@ -113,7 +113,8 @@ class _NumberTileState extends State<NumberTile> {
 
 List<int> initTileList() {
   var rng = Random();
-  List<int> puzzle = List<int>.generate(9, (i) => i); // [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  List<int> puzzle =
+      List<int>.generate(9, (i) => i); // [0, 1, 2, 3, 4, 5, 6, 7, 8]
   int inversions;
 
   do {
